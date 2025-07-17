f109248dafed86c972903e8c73213eba99972e03
shivamkumar854 <shivamkumar3210hjp@gmail.com>
2025-07-17 15:34:37 +0530

    use of mongoDB in backend

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/mongodb.js';
+
+
+// App Config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+// Initialize Middleware
+app.use(express.json())
+app.use(cors())
+    
+// API routes
+app.get('/', (req, res) => res.send('API Working'))
+
+app.listen(PORT, () => console.log('Server is running on port '+PORT))
