05f90b3d2266f57743f4d2e74e7b6969329e8027
shivamkumar854 <shivamkumar3210hjp@gmail.com>
2025-07-19 16:10:07 +0530

    bug fixing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,45 +1,39 @@
- import React from "react";
+import React, { useContext, useEffect } from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
-import { useClerk, useUser, UserButton,} from "@clerk/clerk-react";
+import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
 import { AppContext } from "../context/AppContext";
+
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { isSignedIn } = useUser();
-  const {credit,loadCreditsData} = useContext(AppContext)
+  const { credit, loadCreditsData } = useContext(AppContext);
 
-    useEffect(()=> {
-      if (isSignedIn){
-        loadCreditsData()
-      }
-    },[isSignedIn]
-
-    )
+  useEffect(() => {
+    if (isSignedIn) {
+      loadCreditsData();
+    }
+  }, [isSignedIn]);
 
   return (
     <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
-      
-
       <Link to="/">
-        
         <img className="w-32 sm:w-44" src={assets.logo} alt="" />
       </Link>
 
-      {
-      isSignedIn 
-      ? <div>
-            <UserButton />
+      {isSignedIn ? (
+        <div>
+          <UserButton />
         </div>
-       : <button
+      ) : (
+        <button
           onClick={() => openSignIn({})}
-          className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 text-sm rounded-full mr-[1cm]">
-        
+          className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 text-sm rounded-full mr-[1cm]"
+        >
           Get Started
           <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
         </button>
-      }
-
-      
+      )}
     </div>
   );
 };
